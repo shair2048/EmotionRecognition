8764e37ff43331c293e05144116611681cc57244
SHair <sonhai.it.2048@gmail.com>
2024-06-01 10:31:38 +0700

    undo

--- Recognition.py
+++ Recognition.py
@@ -28,6 +28,7 @@ class EmotionDetector(VideoTransformerBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') 
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
         current_time = time.time()
