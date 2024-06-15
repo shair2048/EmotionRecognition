3606bc56adb00dd3cccab23a90949de5b5fe12aa
SHair <sonhai.it.2048@gmail.com>
2024-06-15 21:53:37 +0700

    Removed transform method from Recognition.py and merged with Orange branch

--- Recognition.py
+++ Recognition.py
@@ -26,12 +26,6 @@ class EmotionDetector(VideoTransformerBase):
         self.last_update_time = time.time()
         self.lock = threading.Lock()
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') 
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
         
     def process_frame(self, frame, show_probabilities=False):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
